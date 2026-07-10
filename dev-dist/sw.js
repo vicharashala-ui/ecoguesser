b35fc977717b56048ecf78e06070d8658e7912ac
Vicharashala <vicharashala@gmail.com>
2026-07-10 10:43:22 +0530

    favicons fixed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-fdf26176'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ic4mjfrj4j4"
+    "revision": "0.m9lfi8dt524"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
