ab319200bde6aa0bf2a3b56b23681988939a512c
Vicharashala <vicharashala@gmail.com>
2026-07-11 10:25:59 +0530

    zoom decreased

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-fdf26176'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hsfgqa3u10o"
+    "revision": "0.kvskfkc7rno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
