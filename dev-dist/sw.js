96e8d7cc267846987d6d862ba571b50211bcfa47
Vicharashala <vicharashala@gmail.com>
2026-07-24 16:36:59 +0530

    jakob krehel e

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-ef3e8fa0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.55agbqasuk"
+    "revision": "0.1fe1gek6mi"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
