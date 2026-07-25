982e4dcef3b607aff01af5bb9c383a57dce95339
Vicharashala <vicharashala@gmail.com>
2026-07-25 08:17:24 +0530

    throbber

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-ef3e8fa0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.1fe1gek6mi"
+    "revision": "0.cgel9pc5h6c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
