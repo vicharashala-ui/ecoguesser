6df128205c72e23166f0ec4ec81ce16f69c144e4
Vicharashala <vicharashala@gmail.com>
2026-08-01 22:38:54 +0530

    fonts

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-ef3e8fa0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.4kkbrqqukeo"
+    "revision": "0.i58npqfpro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
