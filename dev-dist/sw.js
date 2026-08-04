ff362ec8c00a0090ff1ea4f0d24de9b60e5ef565
Vicharashala <vicharashala@gmail.com>
2026-08-04 09:19:23 +0530

    random

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-ef3e8fa0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.7t5l3gdg1dc"
+    "revision": "0.s2tqe0k3a9c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
